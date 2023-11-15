ba9a8590b60aed350bf6eb346b2e988a4d95739e
krishnaamp123 <krishnamahendra1234@gmail.com>
2023-11-15 22:31:55 +0800

    dashboard_commit

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:outsourcing/components/image_slider.dart';
 import 'package:outsourcing/components/text_widget.dart';
 import 'package:outsourcing/list.dart';
 import 'package:outsourcing/pages/see_all.dart';
@@ -58,8 +60,14 @@ class _HomeState extends State<Home> {
                   duration: const Duration(milliseconds: 400),
                   opacity: opacity,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
+                      Image.asset(
+                        'lib/images/logotok.png',
+                        height: 50,
+                        width: 50,
+                      ),
+                      SizedBox(width: 10),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
@@ -69,7 +77,18 @@ class _HomeState extends State<Home> {
                               "Krishna", 25, Colors.black, FontWeight.bold),
                         ],
                       ),
-                      const Icon(Icons.phonelink_ring)
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.notifications,
+                              size: 40, // Ukuran ikon
+                              color: Color.fromRGBO(45, 3, 59, 1), // Warna ikon
+                            )
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -98,133 +117,13 @@ class _HomeState extends State<Home> {
                                   Colors.blue.shade900,
                                   Colors.blue.shade900,
                                 ])),
-                        child: Stack(
-                          children: [
-                            Positioned(
-                                top: 25,
-                                left: 20,
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const CircleAvatar(
-                                      backgroundColor: Colors.white,
-                                      radius: 30,
-                                      child: Center(
-                                        child: Image(
-                                          fit: BoxFit.fill,
-                                          image:
-                                              AssetImage('lib/images/p1.png'),
-                                        ),
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      width: 10,
-                                    ),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        const SizedBox(
-                                          height: 10,
-                                        ),
-                                        TextWidget(
-                                          "You're invited to the live",
-                                          15,
-                                          Colors.white,
-                                          FontWeight.normal,
-                                          letterSpace: 1,
-                                        ),
-                                        const SizedBox(
-                                          height: 5,
-                                        ),
-                                        Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.start,
-                                          children: [
-                                            TextWidget(
-                                              "Stream with  ",
-                                              15,
-                                              Colors.white,
-                                              FontWeight.normal,
-                                              letterSpace: 1,
-                                            ),
-                                            TextWidget(
-                                              "Dr.Navida",
-                                              15,
-                                              Colors.white,
-                                              FontWeight.bold,
-                                              letterSpace: 2,
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    )
-                                  ],
-                                )),
-                            Positioned(
-                                top: 100,
-                                left: 20,
-                                child: Container(
-                                  height: 1,
-                                  width: 300,
-                                  color: Colors.white.withOpacity(.5),
-                                )),
-                            Positioned(
-                                top: 115,
-                                left: 20,
-                                right: 1,
-                                child: Container(
-                                  width: double.infinity,
-                                  child: Row(
-                                    children: [
-                                      TextWidget(
-                                          "120K people join live Stream!",
-                                          15,
-                                          Colors.white,
-                                          FontWeight.bold,
-                                          letterSpace: 1),
-                                      const SizedBox(
-                                        width: 10,
-                                      ),
-                                      const Expanded(
-                                        child: Stack(
-                                          children: [
-                                            Positioned(
-                                              child: CircleAvatar(
-                                                radius: 15,
-                                                backgroundColor: Colors.blue,
-                                              ),
-                                            ),
-                                            Positioned(
-                                              left: 20,
-                                              child: CircleAvatar(
-                                                radius: 15,
-                                                backgroundColor: Colors.red,
-                                              ),
-                                            ),
-                                            Positioned(
-                                              left: 40,
-                                              child: CircleAvatar(
-                                                radius: 15,
-                                                backgroundColor: Colors.white,
-                                              ),
-                                            )
-                                          ],
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                )),
-                            const Positioned(
-                                top: 10,
-                                right: 10,
-                                child: Icon(
-                                  Icons.close_outlined,
-                                  color: Colors.white,
-                                  size: 15,
-                                ))
-                          ],
+                        child: CarouselSlider(
+                          options: CarouselOptions(
+                            autoPlay: true,
+                            aspectRatio: 2.0,
+                            enlargeCenterPage: true,
+                          ),
+                          items: imageSliders,
                         ),
                       ),
                     ),
